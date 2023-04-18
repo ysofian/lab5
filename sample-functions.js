145a5cf9241f0eb839ec8407b4f809cada702843
ysofian <ysofian@calpoly.edu>
2023-04-18 09:24:10 -0700

    done, one potential fix for bug included!

--- sample-functions.js
+++ sample-functions.js
@@ -7,15 +7,14 @@ function div (a, b){
 }
   
 function containsNumbers(text){
-    /*
-    if (text = ' ') {
+    /*if (text == ' ') {
         return false;
-    }
-    */
-    for (let i = 0; i < text.length; i++) {
-        if (!isNaN(text.charAt(i)))
-        return true;
-    }
+    } else { */
+        for (let i = 0; i < text.length; i++) {
+            if (!isNaN(text.charAt(i)))
+            return true;
+        }
+    //}
     return false;
 }
 
