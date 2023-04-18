385be2015fa13776c2a5e8710d5f12ff4e70a672
ysofian <ysofian@calpoly.edu>
2023-04-18 09:11:42 -0700

    need to write tests!

---
+++ sample-functions.js
@@ -0,0 +1,19 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function div (a, b){
+    return a / b;
+}
+  
+function containsNumbers(text){
+    for (let i = 0; i < text.length; i++) {
+        if (!isNaN(text.charAt(i)))
+        return true;
+    }
+    return false;
+}
+
+exports.sum = sum;
+exports.div = div;
+exports.containsNumbers = containsNumbers;
